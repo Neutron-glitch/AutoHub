68a4431f4e754a1a39e50d7cac9bd6705e42a747
ahmedshihab809 <57572719+ahmedshihab809@users.noreply.github.com>
2022-11-18 14:26:42 +0600

    NEW

---
+++ routes/register.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var database = require('../database');
+const { query } = require('../database');
+
+//Register Page
+
+module.exports = router;
